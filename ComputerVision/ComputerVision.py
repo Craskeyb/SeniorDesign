7d60f656a5cd475da22ef08a2752069b02e3318f
HPRP01 <55462701+HPRP01@users.noreply.github.com>
2023-11-07 14:00:31 -0500

    add evlauation and change format of data returned

--- ComputerVision/ComputerVision.py
+++ ComputerVision/ComputerVision.py
@@ -6,8 +6,9 @@ import sys
 import os
 import pandas as pd
 import socket
-
+import json
 class ComputerVision:
+    # Contructor
     def __init__(self):
         self.face_count = FaceCount.FaceCount()
         self.face_extraction = FaceExtraction.FaceExtraction()
@@ -15,18 +16,9 @@ class ComputerVision:
         self.cap = cv2.VideoCapture(0)
         self.cap.set(3, 500)  # Set the width
         self.cap.set(4, 500)  # Set the height
-
-        # TCP_IP = '172.20.10.7'
-        # TCP_PORT = 2222
-        # self.BUFFER_SIZE = 8192
-
-        # self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # self.s.connect((TCP_IP, TCP_PORT))
     
     def get_face_count(self, image):
         count = self.face_count.get_face_count(image)
-
-        print(count, " faces detected")
         return count
     
     def extract_faces(self, image):
@@ -35,30 +27,57 @@ class ComputerVision:
         print(count, " faces extracted")
 
     def get_emotion(self):
-        # TODO: Get inferences on each face from trained CNN
         emotions = self.emotion_detection.classify()
         print("Emotions: ", emotions)
-        return "happy"
+        return emotions
     
+    # TODO: Refine this and clean up unnecessary code
     def get_image(self):
         # TODO: Get image from Raspberry Pi
-        TCP_IP = '172.20.10.7'
+        # TCP_IP = '172.20.10.7'
+        TCP_IP = '192.168.137.99'
+
         TCP_PORT = 2222
         BUFFER_SIZE = 8192
+        
 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(3)
         s.connect((TCP_IP, TCP_PORT))
-
         s.send('R'.encode())
+        data = s.recv(BUFFER_SIZE)
 
-        with open('received_image.jpg', 'wb') as file:
-            while True:
-                data = s.recv(BUFFER_SIZE)
-                if not data:
-                    break
-                file.write(data)
+        rec_data = json.loads(data.decode('utf-8'))
+        rec_data1 = rec_data['Light']
+        rec_data2 = rec_data['Temp']
+        rec_img = rec_data['image_data']
+
+        with open('rec_img.jpg', 'wb') as f:
+            f.write(rec_img)
+
         s.close()
 
+        # print(rec_data1, rec_data2)
+
+        # s.send('R'.encode())
+        # l = s.recv(BUFFER_SIZE)
+        # t = s.recv(BUFFER_SIZE)
+        # print("t", t, "l", l)
+        
+        # l = l.decode('utf-8')
+        # t = t.decode('utf-8')
+
+        # print("t", t, "l", l)
+
+        # with open('received_image.jpg', 'wb') as file:
+        #     while True:
+        #         data = s.recv(BUFFER_SIZE)
+        #         if not data:
+        #             break
+        #         file.write(data)
+        
+
+        # s.close()
         
         img = 'groupImage2.jpg'
         return img
@@ -74,43 +93,55 @@ class ComputerVision:
     def get_data(self): 
         data = {}
 
-        # image   = self.get_image()
-        # pi_data = self.get_pi_data()
+        # ! Comment/uncomment following to either get image from raspberry pi or from webcam
+        # computer_vision.get_image()
+        computer_vision.get_webcam_image()
 
-        # face_count = self.get_face_count(image)
+        # Get count of faces
+        count = computer_vision.get_face_count('received_image.jpg')
 
-        # self.extract_faces(image)
+        # Extract faces from image
+        computer_vision.extract_faces('received_image.jpg')
 
-        # emotions = self.get_emotion()
+        # Get emotions of extracted faces
+        emotions = computer_vision.get_emotion()
 
-        face_count = 0 
-        emotions = "happy"
-        pi_data = {"light": 200, "temperature": 20.1}
+        # Normalize emotions dict to follow standard
+        emotions = computer_vision.norm_emotions(emotions)
 
-        data['count']   = face_count
-        data['emotion'] = emotions
+        # TODO: Need to get actual data from pi
+        pi_data = {"temperature": 20.1, "light": 150}
+
+        data['count']   = count
+        data.update(emotions)
         data.update(pi_data)
         return pd.DataFrame([data])
+    
+    def norm_emotions(self, emotions):
+        for emotion in emotions:
+            # Change from None to 0
+            if emotions[emotion] == None:
+                emotions[emotion] = 0
+            
+        maxEmotion = max(emotions, key= lambda x: emotions[x])
+        for emotion in emotions:
+            # Logic to set max occurring to 1 and then rest to 0
+            if emotion == maxEmotion:
+                emotions[emotion] = 1
+            else:
+                emotions[emotion] = 0
 
+        return emotions
 
+# ! COMMENT THIS OUT IF IMPORTING
 if __name__ == "__main__":
     computer_vision = ComputerVision()
 
     while(True):
-        image = input('Enter image name (or exit to end): ')
-        if image == 'exit':
+        cmd = input('Press enter to process or type \'exit\' to end: ')
+        if cmd == 'exit':
             computer_vision.cap.release()
             sys.exit()
 
-        # if not os.path.exists(image):
-        #     print("Error: File not found")
-        #     continue
-
-        # computer_vision.get_image()
-        computer_vision.get_webcam_image()
-
-        computer_vision.get_face_count('received_image.jpg')
-        computer_vision.extract_faces('received_image.jpg')
-        computer_vision.get_emotion()
         print(computer_vision.get_data())
     
