e3066802d4ee6376e7d4d85464100340adc6e03d
Craskeyb <craskeybt@gmail.com>
2023-11-28 03:04:35 -0500

    Spotify queues 3 songs based on motion

--- SongSelect/SpotifyCalls/spotifyImpl.py
+++ SongSelect/SpotifyCalls/spotifyImpl.py
@@ -1,6 +1,7 @@
 import spotipy
 import SongSelect.SpotifyCalls.credentials as cred
 from spotipy.oauth2 import SpotifyOAuth
+import statistics
 import json
 import time
 
@@ -16,9 +17,9 @@ class RecGenerator:
         self.played = []
 
     #Recommendation Method
-    def makeRecommendation(self, genre):
+    def makeRecommendation(self, genre, motion):
         #Get recommendations from Spotify API
-        recs = self.sp.recommendations(seed_genres=genre, limit=10)
+        recs = self.sp.recommendations(seed_genres=genre, limit=15)
 
         #API Call to determine what genres can be used for recommendations
         ### USED FOR TESTING ###
@@ -30,12 +31,29 @@ class RecGenerator:
         # data = json.dumps(recs,indent=2)
         # print(data)
 
-        #Getting the recommendations that match our tempo evaluation
-        songInfo = []
+        #Getting the tempos of each song to choose recs based on motion
+        songTempos = []
         for song in recs["tracks"]:
-            songInfo.append(self.sp.audio_features(song["uri"])[0]["tempo"])
+            songTempos.append(self.sp.audio_features(song["uri"])[0]["tempo"])
+        
+        print("Song Tempos:")
+        print(songTempos)
 
-        print(songInfo)
+        #After getting tempos, choose the songs that best fit based on amount of motion
+        prunedRecs = []
+        if motion == 'High':
+            while len(prunedRecs) < 3: #If high motion, take the 3 highest tempos from the recs
+                prunedRecs.append(recs["tracks"][songTempos.index(max(songTempos))])
+                songTempos[songTempos.index(max(songTempos))] = 0 #Set val to zero to avoid double selection
+        elif motion == 'Medium':
+            while len(prunedRecs) < 3: #If medium, take the 3 median values from the recs
+                prunedRecs.append(recs["tracks"][songTempos.index(statistics.median(songTempos))])
+                songTempos[songTempos.index(statistics.median(songTempos))] = 0 #Set val to zero to avoid double selection
+        else:
+            while len(prunedRecs) < 3: #If low, take the 3 min values from the recs
+                prunedRecs.append(recs["tracks"][songTempos.index(min(songTempos))])
+                songTempos[songTempos.index(min(songTempos))] = 10**5 #Set value at index of min to be very high so it is not selected twice
+
 
         print("\n\nRecommendations"+
                 "\n-----------------------------------------------")
@@ -44,7 +62,20 @@ class RecGenerator:
             artists = i['artists'][0]['name']
             track = i['name']
 
-            print(artists + " - " + track)  
+            print(artists + " - " + track) 
 
-        self.sp.add_to_queue(recs["tracks"][0]["uri"])
+        print("\n\nRecommendations Chosen based on "+ motion + " motion level" +
+                "\n-----------------------------------------------")
+        #Displaying the 3 recs chosen to be queued based on motion
+        for rec in prunedRecs:
+            artist = rec['artists'][0]['name']
+            track = rec['name']
+            print(artist + " - " + track)
+        
+        #Queueing the pruned songs
+        self.sp.add_to_queue(prunedRecs[0]["uri"])
         self.sp.next_track()  
+        self.sp.add_to_queue(prunedRecs[1]["uri"])
+        self.sp.add_to_queue(prunedRecs[2]["uri"])
+
+        
